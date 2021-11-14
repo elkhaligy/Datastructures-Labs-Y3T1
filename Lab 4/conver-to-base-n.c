77a1aca37cb9312fd2f24813d89a6b2c1ff22478
sirito97 <siritok100@gmail.com>
2021-11-14 15:10:19 +0200

    Bonus problem

---
+++ Lab 4/conver-to-base-n.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node *node;
+struct node
+{
+    int data;
+    struct node *next;
+} * top;
+void push(int x)
+{
+    node new_node = (node)malloc(sizeof(struct node));
+    new_node->data = x;
+    new_node->next = top;
+    top = new_node;
+}
+void pop()
+{
+    node p;
+    //int x = -1;
+    if (top == NULL)
+        // printf("Empty\n");
+        return;
+    else
+    {
+        p = top;
+        top = top->next;
+        //x = p->data;
+        free(p);
+    }
+    //return x;
+}
+void display()
+{
+    node p = top;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+void to_base_n(int n, int b)
+{
+    if (n == 0)
+        return;
+    int q = n % b;
+    push(q);
+    to_base_n(n / b, b);
+    // while (n > 0)
+    // {
+    //     push(n % b);
+    //     n = n / b;
+    // }
+}
+int main(void)
+{
+    int n, b;
+    scanf("%d%d", &n, &b);
+    to_base_n(n, b);
+    display();
+    return 0;
+}
