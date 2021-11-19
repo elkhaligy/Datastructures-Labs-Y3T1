802907bc5e029e51d351cf7fdbd6f2de440edd26
sirito97 <siritok100@gmail.com>
2021-11-19 17:22:51 +0200

    added queue

--- Lab 5/courses.c
+++ Lab 5/courses.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+} *front = NULL, *rear = NULL;
 
+void enqueue(int x)
+{
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    if (new == NULL)
+        printf("Queue is full");
+    else
+    {
+        new->data = x;
+        new->next = NULL;
+        if (front == NULL)
+        {
+            //It's first node
+            front = rear = new;
+        }
+        else
+        {
+            //Insert a new node normally
+            rear->next = new;
+            rear = new;
+        }
+    }
+}
+int dequeue()
+{
+    int x = -1;
+    struct node *bye;
+    if (front == NULL)
+    {
+    }
+    else
+    {
+        x = front->data;
+        bye = front;
+        front = front->next;
+        free(bye);
+    }
+    return x;
+}
+void display()
+{
+    struct node *p = front;
+    while (p != NULL)
+    {
+
+        printf("%d,", p->data);
+        p = p->next;
+    }
+}
 int main(void)
 {
+
     return 0;
 }
