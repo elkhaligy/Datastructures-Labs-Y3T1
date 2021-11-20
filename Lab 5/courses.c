7ea483edbeab292d1b15ff5825c6f25f539d2012
sirito97 <siritok100@gmail.com>
2021-11-20 05:20:15 +0200

    Update courses.c

--- Lab 5/courses.c
+++ Lab 5/courses.c
@@ -54,8 +54,70 @@ void display()
         p = p->next;
     }
 }
+
 int main(void)
 {
-
+    int n, d, t;
+    int *p;
+    int *q;
+    int cnt1 = 0, cnt2 = 0;
+    scanf("%d", &n);
+    p = (int *)malloc(n * 2 * sizeof(int));
+    q = (int *)malloc(n * 2 * sizeof(int));
+    for (int i = 0; i < n * 2; i += 2)
+    {
+        scanf("%d%d", &d, &t);
+        p[i] = t;
+        p[i + 1] = d;
+        // enqueue(t);
+        //enqueue(d);
+    }
+    for (int i = 0; i < n * 2; i += 2)
+    {
+        q[i] = 0;
+    }
+    int min = 2000;
+    int min_index;
+    int i;
+    for(int k=0;k<n;k++)
+    {
+        for (i = 0; i < n * 2; i += 2)
+        {
+            if (p[i] < min && q[i]!=1)
+            {
+                min = p[i];
+                min_index = i;
+            }
+        }
+        enqueue(p[min_index]);
+        enqueue(p[min_index+1]);
+        q[min_index]=1;
+        min=2000;
+    }
+    
+    for (int i = 0; i < n * 2; i++)
+        printf("%d ", p[i]);
+    //display();
+    //printf("\n");
+    while (front != NULL)
+    {
+        if (cnt1 == 0)
+        {
+            dequeue();
+            cnt1 += dequeue();
+            cnt2 += 1;
+            continue;
+        }
+        int x = dequeue();
+        int y = dequeue();
+        if (cnt1 + y <= x)
+        {
+            cnt1 += y;
+            cnt2 += 1;
+        }
+        //display();
+        //printf("\n");
+    }
+    printf("%d", cnt2);
     return 0;
 }
