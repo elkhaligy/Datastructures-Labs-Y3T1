d24f37ebbd99196ed6bbd3a42762699be29cbd74
sirito97 <66971290+sirito97@users.noreply.github.com>
2021-12-04 02:19:52 +0200

    Update is_full_binary_tree.c

--- Lab 6/is_full_binary_tree.c
+++ Lab 6/is_full_binary_tree.c
@@ -3,20 +3,34 @@
 #include <math.h>
 #include <stdbool.h>
 
-typedef struct node *Node;
-struct node
+struct node_tree
 {
-    struct node *data;
-    struct node *next;
-
+    /**
+     * Summary: This is the tree node which has an integer data and two pointers
+     *          rchild pointer holds a node_tree address and same for lchild
+     */
     int node_data;
-    struct node *rchild;
-    struct node *lchild;
+    struct node_tree *rchild;
+    struct node_tree *lchild;
+};
+struct node_queue
+{
+    /**
+     * Summary: This is the queue node, which has a data of datatype node_tree pointer
+     *          and a pointer next  which holds an address of node_queue for the next node in queue
+     */
+    struct node_tree *data;
+    struct node_queue *next;
 };
 
-void enqueue(struct node **front, struct node **rear, struct node *x)
+// Start Queue operations
+void enqueue(struct node_queue **front, struct node_queue **rear, struct node_tree *x)
 {
-    struct node *new = (struct node *)malloc(sizeof(struct node));
+    /**
+     * Queue eqnueue function, takes input front and rear pointers passed by reference
+     * The data stored in this queue is of type struct node_tree pointer
+     */
+    struct node_queue *new = (struct node_queue *)malloc(sizeof(struct node_queue));
     if (new == NULL)
         printf("Queue is full");
     else
@@ -36,10 +50,16 @@ void enqueue(struct node **front, struct node **rear, struct node *x)
         }
     }
 }
-struct node *dequeue(struct node **front)
+
+struct node_tree *dequeue(struct node_queue **front)
 {
-    struct node *x = NULL;
-    struct node *bye;
+    /**
+     * This function dequeue an element from the front of the queue
+     * The argument passed is front pointer of the queue and it is passed by reference
+     * The function returns variable of type struct node_tree * to be used later in the tree creation
+     */
+    struct node_tree *x = NULL;
+    struct node_queue *bye;
     if (front == NULL)
     {
         return x;
@@ -53,28 +73,43 @@ struct node *dequeue(struct node **front)
     }
     return x;
 }
-int is_empty_queue(struct node *front)
+
+int is_empty_queue(struct node_queue *front)
 {
+    /**
+     * This function checks if the queue in empty or not
+     * 
+     */
     if (front == NULL)
         return 1;
     return 0;
 }
+// End Queue operations
 
-struct node *create(int n)
+// Start Tree creation
+struct node_tree *create(int n)
 {
-    n--;
-    struct node *front = NULL;
-    struct node *rear = NULL;
-
-    struct node *p;
-    struct node *t;
-    struct node *root;
+    /**
+     * This function creates a binary tree
+     * the argument passed in integer n which is the number of nodes in the tree
+     * The function retuns struct node_tree * type variable which is later we call it root
+     */
+    struct node_queue *front = NULL;
+    struct node_queue *rear = NULL;
+    struct node_tree *p;
+    struct node_tree *t;
+    struct node_tree *root;
     int x;
     //printf("Enter root value: ");
     scanf("%d", &x);
-    root = (struct node *)malloc(sizeof(struct node));
+    root = (struct node_tree *)malloc(sizeof(struct node_tree));
     root->node_data = x;
     root->lchild = root->rchild = 0;
+    n--;
+    // Main idea here is using a queue and eqnueue the pointer of each node to it
+    // and then dequeue it and fill it's left child and right child
+    // repeating this step and by queue nature we will successfully fill a binary tree
+    // from left to right as usual
     enqueue(&front, &rear, root);
     while (n != 0)
     {
@@ -83,7 +118,7 @@ struct node *create(int n)
         scanf("%d", &x);
         if (x != -1)
         {
-            t = (struct node *)malloc(sizeof(struct node));
+            t = (struct node_tree *)malloc(sizeof(struct node_tree));
             t->node_data = x;
             t->lchild = t->rchild = 0;
             p->lchild = t;
@@ -93,7 +128,7 @@ struct node *create(int n)
         scanf("%d", &x);
         if (x != -1)
         {
-            t = (struct node *)malloc(sizeof(struct node));
+            t = (struct node_tree *)malloc(sizeof(struct node_tree));
             t->node_data = x;
             t->lchild = t->rchild = 0;
             p->rchild = t;
@@ -103,52 +138,56 @@ struct node *create(int n)
     }
     return root;
 }
-void pre_order(struct node *root)
-{
-    if (root)
-    {
-        printf("%d ", root->node_data);
-        pre_order(root->lchild);
-        pre_order(root->rchild);
-    }
-}
-void in_order(struct node *root)
-{
-    if (root)
-    {
-        in_order(root->lchild);
-        printf("%d ", root->node_data);
-        in_order(root->rchild);
-    }
-}
-void post_order(struct node *root)
-{
-    if (root)
-    {
-        post_order(root->lchild);
-        post_order(root->rchild);
-        printf("%d ", root->node_data);
-    }
-}
-int height(struct node *root)
-{
-    int x = 0, y = 0;
-    if (root == 0)
-        return 0;
-    x = height(root->lchild);
-    y = height(root->rchild);
-    if (x > y)
-        return x + 1;
-    else
-        return y + 1;
-}
-int count(struct node *root)
-{
-    if (root)
-        return count(root->lchild) + count(root->rchild) + 1;
-    return 0;
-}
-bool is_full_tree(struct node *root)
+// End Tree creation
+
+// Start Tree operations
+// void pre_order(struct node_tree *root)
+// {
+//     if (root)
+//     {
+//         printf("%d ", root->node_data);
+//         pre_order(root->lchild);
+//         pre_order(root->rchild);
+//     }
+// }
+// void in_order(struct node_tree *root)
+// {
+//     if (root)
+//     {
+//         in_order(root->lchild);
+//         printf("%d ", root->node_data);
+//         in_order(root->rchild);
+//     }
+// }
+// void post_order(struct node_tree *root)
+// {
+//     if (root)
+//     {
+//         post_order(root->lchild);
+//         post_order(root->rchild);
+//         printf("%d ", root->node_data);
+//     }
+// }
+// int height(struct node_tree *root)
+// {
+//     int x = 0, y = 0;
+//     if (root == 0)
+//         return 0;
+//     x = height(root->lchild);
+//     y = height(root->rchild);
+//     if (x > y)
+//         return x + 1;
+//     else
+//         return y + 1;
+// }
+// int count(struct node_tree *root)
+// {
+//     if (root)
+//         return count(root->lchild) + count(root->rchild) + 1;
+//     return 0;
+// }
+
+bool is_full_tree(struct node_tree *root)
 {
     if (root == NULL)
         return true;
@@ -156,9 +195,10 @@ bool is_full_tree(struct node *root)
         return true;
     if ((root->lchild) && (root->rchild))
         return (is_full_tree(root->lchild) && is_full_tree(root->rchild));
-
     return false;
 }
+// End Tree operations
+
 int main(void)
 {
     int n;
@@ -168,15 +208,7 @@ int main(void)
         printf("NO");
         return 0;
     }
-    struct node *root = create(n);
-    // int tree_height = height(root);
-    // tree_height--;
-    //printf("%d \n",count(root));
-    //printf("%f \n", pow((double)2, (double)(tree_height + 1)) - 1);
-    // if ((pow((double)2, (double)(tree_height + 1)) - 1) == count(root))
-    //     printf("YES");
-    // else
-    //     printf("NO");
+    struct node_tree *root = create(n);
     if (is_full_tree(root))
         printf("YES");
     else
